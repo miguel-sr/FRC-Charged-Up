4f938e533f703f31f43bfb16e2fab8aa0a95f291
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-04-04 20:06:59 -0300

    :sparkles: feat: Add MaxSpeed Constants and OIConstants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,5 +18,34 @@ public final class Constants {
     public static final int kLeftRearDriveMotorPort = 2;
     public static final int kRightFrontDriveMotorPort = 3;
     public static final int kRightRearDriveMotorPort = 4;
+
+    public static final double kYLowSpeedLimiter = 0.5;
+    public static final double kTurningLowSpeedLimiter = 0.6;
+    public static final double kYHighSpeedLimiter = 1.0;
+    public static final double kTurningHighSpeedLimiter = 0.75;
+  }
+
+  public static final class OIConstants {
+    public static final int kDriverJoystick = 0;
+
+    public static final class Axis {
+      public static final int kLeftStickX = 0;
+      public static final int kLeftStickY = 1;
+      public static final int kRightStickX = 4;
+      public static final int kRightStickY = 5;
+      public static final int kLeftTrigger = 2;
+      public static final int kRightTrigger = 3;
+    }
+
+    public static final class Buttons {
+      public static final int kA = 1;
+      public static final int kB = 2;
+      public static final int kX = 3;
+      public static final int kY = 4;
+      public static final int kLeftBumper = 5;
+      public static final int kRightBumper = 6;
+      public static final int kBack = 7;
+      public static final int kStart = 8;
+    }
   }
 }
