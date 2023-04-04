4c151307f84a6dafac62d188c8085c121eec9d0d
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-04-04 20:22:53 -0300

    :sparkles: feat: Add bindings to Arm Subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.DriveJoystickCmd;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
 
   private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+  private final ArmSubsystem armSubsystem = new ArmSubsystem();
 
-  private final Joystick driverJoystick = new Joystick(OIConstants.kDriverJoystick);
+  private final Joystick driverJoystick = new Joystick(OIConstants.kDriverControllerPort);
+  private final Joystick armController = new Joystick(OIConstants.kArmControllerPort);
 
   public RobotContainer() {
     driveSubsystem.setDefaultCommand(new DriveJoystickCmd(
@@ -28,7 +34,22 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+    new JoystickButton(armController, OIConstants.Buttons.kA)
+    .onTrue(Commands.runOnce(
+      () -> {
+        armSubsystem.setGoal(2);
+        armSubsystem.enable();
+      }, 
+      armSubsystem));
 
+    // Move the arm to neutral position when the 'B' button is pressed.
+    new JoystickButton(armController, OIConstants.Buttons.kB)
+      .onTrue(Commands.runOnce(
+        () -> {
+          armSubsystem.setGoal(ArmConstants.kArmOffsetRads);
+          armSubsystem.enable();
+        }, 
+        armSubsystem));
   }
 
   public Command getAutonomousCommand() {
