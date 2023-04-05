7fccbcfb12b1166ad8d0e5b6a409995a6fd8ec4b
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-04-05 14:32:59 -0300

    :sparkles: feat: Add command to drive using encoders

---
+++ src/main/java/frc/robot/commands/DriveWithEncoders.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class DriveWithEncoders extends CommandBase {
+  DriveSubsystem driveSubsystem;
+  private final double distance, speed;
+
+  public DriveWithEncoders(DriveSubsystem driveSubsystem, double distance, double speed) {
+    this.driveSubsystem = driveSubsystem;
+    this.distance = distance;
+    this.speed = speed;
+
+    addRequirements(driveSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+    driveSubsystem.resetEncoders();
+  }
+
+  @Override
+  public void execute() {    
+    driveSubsystem.curvatureDrive(-speed, 0, true);
+  }
+  @Override
+  public void end(boolean interrupted) {
+    driveSubsystem.stop();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return driveSubsystem.getAverageEncoderDistance() > distance;
+  }
+}
