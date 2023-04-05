442ea42513cdcbe4f999ce84a52df2e427e74295
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-04-05 14:36:50 -0300

    :sparkles: feat: Add command to drive using encoders

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -2,7 +2,11 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -30,6 +34,21 @@ public class DriveSubsystem extends SubsystemBase {
     private final DifferentialDrive drive = 
         new DifferentialDrive(leftMotors, rightMotors);
 
+    private final Encoder leftEncoder = new Encoder(
+        DriveConstants.Encoders.kLeftEncoderPorts[0], 
+        DriveConstants.Encoders.kLeftEncoderPorts[1], 
+        false, 
+        EncodingType.k4X);
+
+    private final Encoder rightEncoder = new Encoder(
+        DriveConstants.Encoders.kRightEncoderPorts[0], 
+        DriveConstants.Encoders.kRightEncoderPorts[1], 
+        true, 
+        EncodingType.k4X);
+
+    private final AHRS gyro =
+        new AHRS(SPI.Port.kMXP);
+
     public DriveSubsystem() {
         leftFront.setNeutralMode(NeutralMode.Brake);
         leftRear.setNeutralMode(NeutralMode.Brake);
@@ -38,12 +57,37 @@ public class DriveSubsystem extends SubsystemBase {
     
         leftMotors.setInverted(false);
         rightMotors.setInverted(true);
+
+        leftEncoder.setDistancePerPulse(DriveConstants.Encoders.kEncodersDistancePerPulse);
+        rightEncoder.setDistancePerPulse(DriveConstants.Encoders.kEncodersDistancePerPulse);
     }
 
     @Override
     public void periodic() {}
 
+    public void resetEncoders() {
+        leftEncoder.reset();
+        rightEncoder.reset();
+    }
+
+    public double getAverageEncoderDistance() {
+        return Math.abs((leftEncoder.getDistance() + rightEncoder.getDistance()) / 2);
+    }
+
+    public void resetGyro() {
+        gyro.reset();
+        gyro.calibrate();
+    }
+
+    public double getAngle() {
+        return -gyro.getAngle();
+    }
+
     public void curvatureDrive(double xSpeed, double zRotation, boolean isQuickTurn) {
         drive.curvatureDrive(xSpeed, zRotation, isQuickTurn);
+    }
+
+    public void stop() {
+        drive.stopMotor();
     }
 }
